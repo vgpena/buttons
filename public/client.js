3b5838dfe1c8fad453a16303fde52e001e3e602d
Violet Peña <violet@instrument.com>
2017-07-24 11:35:12 -0700

    Start/stop playback buttons.

--- public/client.js
+++ public/client.js
@@ -6,15 +6,18 @@ const fileUrl = 'buttons.mp3';
 const context = new AudioContext();
 const playButton = document.getElementById('button');
 let buffer = null;
+const source = context.createBufferSource();
 
 window.fetch(fileUrl)
     .then(response => response.arrayBuffer())
     .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
     .then(audioBuffer => {
         buffer = audioBuffer;
+        source.buffer = buffer;
+        source.start();
     });
 
-playButton.onclick = () => play();
+// playButton.onclick = () => play();
 
 function play() {
     const source = context.createBufferSource();
@@ -23,9 +26,17 @@ function play() {
     source.start();
 }
 
+function parseButtonData(data) {
+    if (data.b0) {
+        source.connect(context.destination);
+    } else if (data.b1) {
+        source.disconnect(context.destination);
+    }
+}
+
 // ===== interaction / button events
 socket.on('buttonUpdate', (data) => {
     if (data.id === 9) {
-        console.log(data);
+        parseButtonData(data);
     }
 });
