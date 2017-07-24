c66e60fd8c4d74c4119f1d77f3d7855c20aaa9f4
Violet Peña <violet@instrument.com>
2017-07-24 11:26:59 -0700

    Play song when click button.

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-
-// ========================
 
 app.listen(8080);
 
@@ -14,25 +10,3 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static('public'));
-
-// ========================
-
-// function sendClientsCount() {
-//   users.clients((err, clients) => {
-//     if (err) {
-//       throw new Error(err);
-//     }
-//     users.emit('updateClientCount', clients.length);
-//     viewer.emit('updateClientCount', clients.length);
-//   });
-// }
-
-// function addSocketEvents(socket) {
-//   socket.on('click', (data) => {
-//     viewer.emit('click', data);
-//   });
-
-//   socket.on('disconnect', () => {
-//     sendClientsCount();
-//   });
-// }
--- public/client.js
+++ public/client.js
@@ -1,7 +1,31 @@
 const socket = io.connect('10.0.0.145:3000/');
+const myButtonID = 9;
+const fileUrl = 'buttons.mp3';
 
+// ==== load & play file
+const context = new AudioContext();
+const playButton = document.getElementById('button');
+let buffer = null;
+
+window.fetch(fileUrl)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+        buffer = audioBuffer;
+    });
+
+playButton.onclick = () => play();
+
+function play() {
+    const source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start();
+}
+
+// ===== interaction / button events
 socket.on('buttonUpdate', (data) => {
-    console.log(data);
+    if (data.id === 9) {
+        console.log(data);
+    }
 });
-
-console.log('a');
