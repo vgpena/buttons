73d4b01bcf1e62bbb5addba84f42d91b5ae27398
Violet Peña <violet@instrument.com>
2017-07-24 13:41:09 -0700

    Good timestamps.

--- public/client.js
+++ public/client.js
@@ -3,21 +3,47 @@ const myButtonID = 9;
 const fileUrl = 'buttons.mp3';
 
 const track = new Pizzicato.Sound(fileUrl);
+let currTrackTime = 0;
+let currTime = Pizzicato.context.currentTime;
+let loops = [];
+let currLoop = {
+    start: 0,
+    stop: 0,
+};
+
+function loop() {
+    if (!track.playing) {
+        return;
+    }
+    setTrackTime();
+    // create new loop
+    if (currLoop.start === currLoop.stop) {
+        currLoop.start = currTrackTime;
+    } else {
+        currLoop.stop = currTrackTime;
+        loops.push({ ...currLoop });
+        currLoop = {
+            start: 0,
+            stop: 0,
+        }
+        console.log(JSON.stringify(loops));
+    }
+}
+
+function setTrackTime() {
+    currTrackTime += Pizzicato.context.currentTime - currTime;
+}
 
 // // ===== interaction / button events
 function parseButtonData(data) {
     if (data.b0) {
-        track.play(0, 5);
+        track.play();
+        currTime = Pizzicato.context.currentTime;
     } else if (data.b1) {
         track.pause();
+        currTime = Pizzicato.context.currentTime;
     } else if (data.b2) {
-        var tremolo = new Pizzicato.Effects.Tremolo({
-            speed: 20,
-            depth: 0.8,
-            mix: 0.8
-        });
-
-        track.addEffect(tremolo);
+        loop();
     }
 }
 
