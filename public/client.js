9fded5f578576b70dd047e61f4edf96967914d0f
Violet Peña <violet@instrument.com>
2017-07-24 14:41:05 -0700

    Better

--- public/client.js
+++ public/client.js
@@ -2,61 +2,86 @@ const socket = io.connect('10.0.0.145:3000/');
 const myButtonID = 9;
 const fileUrl = 'buttons.mp3';
 
-const track = new Pizzicato.Sound(fileUrl);
-var delay = new Pizzicato.Effects.Delay({
-    gain: 0.4
-});
-track.addEffect(delay);
-const tracks = new Pizzicato.Group();
+// const track = new Pizzicato.Sound(fileUrl);
+const context = new AudioContext();
+let buffer = null;
+const source = context.createBufferSource();
 
-tracks.addSound(track);
+const tracks = [source];
+let playing = false;
 
-let currTrackTime = 0;
-let currTime = Pizzicato.context.currentTime;
+let startTime = context.currentTime;
+let currentTime = 0;
+
 let loops = [];
 let currLoop = {
     start: 0,
     stop: 0,
 };
 
+function play() {
+    playing = true;
+    tracks.forEach((src) => {
+        src.connect(context.destination);
+    });
+}
+
+function pause() {
+    playing = false;
+    tracks.forEach((src) => {
+        src.disconnect(context.destination);
+    });
+}
+
 function loop() {
-    if (!track.playing) {
+    if (!playing) {
         return;
     }
-    setTrackTime();
-    // create new loop
+
     if (currLoop.start === currLoop.stop) {
-        currLoop.start = currTrackTime;
+        currLoop.start = currentTime;
     } else {
-        currLoop.stop = currTrackTime;
-        loops.push({ ...currLoop });
+        currLoop.stop = currentTime;
+        const src = context.createBufferSource();
+        src.buffer = buffer;
+        src.loop = true;
+        src.loopStart = 0;
+        src.loopEnd = 3;
+        src.start();
+
+        tracks.push(src);
+        play();
+
         currLoop = {
             start: 0,
             stop: 0,
-        }
-        const newTrack = track.clone();
-        // newTrack.frequency = 440 * 8;
-        tracks.addSound(newTrack);
-        newTrack.play(loops[loops.length - 1].start);
+        };
     }
-}
-
-function setTrackTime() {
-    currTrackTime += Pizzicato.context.currentTime - currTime;
 }
 
 // // ===== interaction / button events
 function parseButtonData(data) {
     if (data.b0) {
-        tracks.play();
-        currTime = Pizzicato.context.currentTime;
+        play();
     } else if (data.b1) {
-        tracks.pause();
-        currTime = Pizzicato.context.currentTime;
+        pause();
     } else if (data.b2) {
         loop();
     }
 }
+
+// setup
+
+window.fetch(fileUrl)
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+        buffer = audioBuffer;
+        source.buffer = buffer;
+        source.loop = true;
+        source.start();
+        // console.log(audioBuffer.duration);
+    });
 
 socket.on('buttonUpdate', (data) => {
     if (data.id === 9) {
