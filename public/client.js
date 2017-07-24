634b9a02e835f2303ccc4e079b65a566a1c2fbf1
Violet Peña <violet@instrument.com>
2017-07-24 12:31:19 -0700

    trying out Pizzicato

--- public/client.js
+++ public/client.js
@@ -2,39 +2,25 @@ const socket = io.connect('10.0.0.145:3000/');
 const myButtonID = 9;
 const fileUrl = 'buttons.mp3';
 
-// ==== load & play file
-const context = new AudioContext();
-const playButton = document.getElementById('button');
-let buffer = null;
-const source = context.createBufferSource();
+const track = new Pizzicato.Sound(fileUrl);
 
-window.fetch(fileUrl)
-    .then(response => response.arrayBuffer())
-    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
-    .then(audioBuffer => {
-        buffer = audioBuffer;
-        source.buffer = buffer;
-        source.start();
-    });
-
-// playButton.onclick = () => play();
-
-function play() {
-    const source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.start();
-}
-
+// // ===== interaction / button events
 function parseButtonData(data) {
     if (data.b0) {
-        source.connect(context.destination);
+        track.play(0, 5);
     } else if (data.b1) {
-        source.disconnect(context.destination);
+        track.pause();
+    } else if (data.b2) {
+        var tremolo = new Pizzicato.Effects.Tremolo({
+            speed: 20,
+            depth: 0.8,
+            mix: 0.8
+        });
+
+        track.addEffect(tremolo);
     }
 }
 
-// ===== interaction / button events
 socket.on('buttonUpdate', (data) => {
     if (data.id === 9) {
         parseButtonData(data);
